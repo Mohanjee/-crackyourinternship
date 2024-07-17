4fe3c93b0a288fc2de927c12551c077e1fd32acf
Mohanjee Narise <127100570+Mohanjee@users.noreply.github.com>
2024-07-17 20:54:40 +0530

    Create sortColors.cpp

---
+++ Arrays/sortColors.cpp
@@ -0,0 +1,33 @@
+//bruteforce(sorting generally)
+void sortColors(vector<int> & nums){
+  sort(nums.begin(),nums.end());
+}
+//T(n) : O(nlogn) because of merge sort
+//S(n) : O(1)
+
+//better 
+ void sortColors(vector<int>& nums) {
+        int cnt0 =0;
+        int cnt1=0;
+        int cnt2 =0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0){
+                cnt0++;
+            }if(nums[i]==1){
+                cnt1++;
+            }if(nums[i]==2){
+                cnt2++;
+            }
+        }
+        for(int i=0;i<cnt0;i++){
+            nums[i]=0;
+        }for(int i=cnt0;i<cnt0+cnt1;i++){
+            nums[i]=1;
+        }for(int i=cnt0+cnt1;i<nums.size();i++){
+            nums[i]=2;
+        }
+    }
+//T(n) : O(n)
+//S(n) : O(1)
+
+//optimal
