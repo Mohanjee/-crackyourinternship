fa60989549e77a0f3db678f71caf2b715e1ce205
Mohanjee Narise <127100570+Mohanjee@users.noreply.github.com>
2024-07-17 13:43:27 +0530

    Update findDuplicate.cpp

--- Arrays/findDuplicate.cpp
+++ Arrays/findDuplicate.cpp
@@ -1,4 +1,19 @@
 //brute force 
+int findDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        for(int i=0;i<=n;i++){
+            for(int j = i+1;j<=n;j++){
+                if(nums[i]== nums[j]){
+                    return nums[i];
+                }
+            }
+        }
+        return -1;
+    }
+//T(n) : O(n^2)
+//S(n) : O(1)
+
+//Better
  int findDuplicate(vector<int>& nums) {
         int n = nums.size();
         sort(nums.begin(),nums.end());
@@ -7,3 +22,24 @@
         }
         return -1;
     }
+//T(n) : O(nlogn)
+//S(n) : O(1)
+
+//Optimal 
+int findDuplicate(vector<int>& nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow != fast);
+
+        fast = nums[0];
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+//T(n) : O(n)
+//S(n) : O(1)
